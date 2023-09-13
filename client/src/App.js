fbaec63b41f03775597daae4ff3fcd50ce5cfbb4
FRANZ RHANICO <109740904+rhanico@users.noreply.github.com>
2023-09-12 22:30:55 -0700

    fixed issues with backend connectivity to front end, added fetch/setloading

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,42 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import fecth from 'isomorphic-fetch';
 
 function App() {
+    const [ userData, setUserData ] = useState ([]);
+    const [ loading, setLoading ] = useState(true);
+
+    useEffect(() => {
+
+      fecth('/api')
+                                   // will "FETCH" data from my back-end express server. //
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error( 'New Response Was Not Ok');
+          }
+          return response.json();
+        })
+        .then((data) => {
+          setUserData(data.user)
+          setLoading(false);       // set LOADING to false once data is fetched 
+        })
+        .catch((error) => {
+          console.error('Error Fetching Data:', error);
+          setLoading(false);
+        });
+    }, []);
 
   return (
     <div>
       <h1> Hello Word! </h1>
+      {loading ? (
+        <p> LOADING ... </p>
+      ) : (
+        <ul>
+          {userData.map (( user, index ) => (
+            <li key = {index}> {user} </li>
+          ))}
+        </ul>
+      )}
 
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ const express = require( 'express' )
 const app = express()
 
 app.get("/api", (req, res) => {
-    res.json({"user": ["userOne","userTwo","userThree"] })
 })
 
 app.listen(3000, () => {console.log("Server started on port 3000") })
